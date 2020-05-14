180c14b3014cd507f4a1c3a072ad18408d540b59
Drake <drakedemuyt@gmail.com>
2020-05-14 00:47:16 -0400

    Fixing linter issues

--- Index.js
+++ Index.js
@@ -1,97 +1,96 @@
 
-let player = {
-    health: 100,
-    power: 20,
-    weapon: 10
-}
-const pAttack = player.power + player.weapon
+const player = {
+  health: 100,
+  power: 20,
+  weapon: 10,
+};
+const pAttack = player.power + player.weapon;
 
-let opponent = {
-    health: 100,
-    power: 20,
-    weapon: 10
-}
-const oAttack = opponent.power + opponent.weapon
-
+const opponent = {
+  health: 100,
+  power: 20,
+  weapon: 10,
+};
+const oAttack = opponent.power + opponent.weapon;
 
 
 const attack = () => {
-    let attackButton = document.getElementById('attack-button');
-    let healthButton = document.getElementById('health-button');
-    let restartButton = document.getElementById('restart-button');
-    let gameMessage = document.getElementById('game-message');
+  const attackButton = document.getElementById('attack-button');
+  const healthButton = document.getElementById('health-button');
+  const restartButton = document.getElementById('restart-button');
+  const gameMessage = document.getElementById('game-message');
 
-    // let playerAttack = determineAttack(player.power)
-    let playerAttack = determineAttack(pAttack)
-    opponent.health -= playerAttack;
+  // let playerAttack = determineAttack(player.power)
+  const playerAttack = determineAttack(pAttack);
+  opponent.health -= playerAttack;
+  printToScreen();
+
+  if (isGameOver(opponent.health)) {
+    opponent.health = 0;
     printToScreen();
+    endGame('Congrats, you won son!');
+    return;
+  }
 
-    if (isGameOver (opponent.health)){
-        opponent.health = 0
-        printToScreen()
-        endGame("Congrats, you won son!");
-        return;
-    }
-
-    let healthPotion = 
+  const healthPotion =
 
     attackButton.disabled = true;
-    gameMessage.innerText = "Its rumble time!"
+  gameMessage.innerText = 'Its rumble time!';
 
-    setTimeout(() =>{
-        // let opponentAttack = determineAttack(opponent.power)
-        let opponentAttack = determineAttack(oAttack)
-        player.health -= opponentAttack;
-        printToScreen();
+  setTimeout(() =>{
+    // let opponentAttack = determineAttack(opponent.power)
+    const opponentAttack = determineAttack(oAttack);
+    player.health -= opponentAttack;
+    printToScreen();
 
-        if (isGameOver (player.health)){
-            player.health = 0
-            printToScreen()
-            endGame("You are complete trash, how could you lose?");
-            return;
-        }
-        attackButton.disabled = false;
-    }, 500);
+    if (isGameOver(player.health)) {
+      player.health = 0;
+      printToScreen();
+      endGame('You are complete trash, how could you lose?');
+      return;
+    }
+    attackButton.disabled = false;
+  }, 500);
 
-    console.log(playerAttack)
-}
+  console.log(playerAttack);
+};
 
 const endGame = (message) => {
-    document.getElementById('game-message').innerText = message;
-    document.getElementById('attack-button').hidden = true;
-    document.getElementById('restart-button').hidden = false;
-}
+  document.getElementById('game-message').innerText = message;
+  document.getElementById('attack-button').hidden = true;
+  document.getElementById('restart-button').hidden = false;
+};
 
 const determineAttack = (attack) => {
-    return Math.floor(Math.random() * attack);
-    // return Math.floor(Math.random() * power + weapon);
-}
+  return Math.floor(Math.random() * attack);
+  // return Math.floor(Math.random() * power + weapon);
+};
 
 
 const isGameOver = (health) => {
-    return health <= 0;
-}
+  return health <= 0;
+};
 
 const restart = () => {
-    let attackButton =  document.getElementById('attack-button');
-    player.health = 100;
-    opponent.health = 100;
-    document.getElementById('game-message').innerText = "";
-    attackButton.disabled = false;
-    attackButton.hidden = false;
-    document.getElementById('restart-button').hidden = true;
-    printToScreen()
-}
+  const attackButton = document.getElementById('attack-button');
+  player.health = 100;
+  opponent.health = 100;
+  document.getElementById('game-message').innerText = '';
+  attackButton.disabled = false;
+  attackButton.hidden = false;
+  document.getElementById('restart-button').hidden = true;
+  printToScreen();
+};
 
 const printToScreen = () => {
-    document.getElementById('opponent-health').innerText = 
+  document.getElementById('opponent-health').innerText =
     opponent.health;
 
-    document.getElementById('player-health').innerText = 
+  document.getElementById('player-health').innerText =
     player.health;
 
-    document.getElementById('player-attack').innerText = 
+  document.getElementById('player-attack').innerText =
     pAttack;
-}
+};
 
 printToScreen();
