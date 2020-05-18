d50da35ea600de6f2f64b9ee20f4187487f7c94a
Drake <drakedemuyt@gmail.com>
2020-05-18 17:51:19 -0400

    Fixing rerender hero, potion button diable, level counter bugs

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const session = require('express-session');
 // Requiring passport as we've configured it
 const passport = require('./config/passport');
+const Chart = require('chart.js');
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
